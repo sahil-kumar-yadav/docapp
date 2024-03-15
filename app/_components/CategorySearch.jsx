0c6f3f688edaa5dc94c25bc197516142d3519503
SAHIL YADAV <121011155+yadavsk098@users.noreply.github.com>
2024-03-15 18:38:31 +0530

    updated

---
+++ app/_components/CategorySearch.jsx
@@ -0,0 +1,22 @@
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Search } from 'lucide-react'
+import React from 'react'
+
+const CategorySearch = () => {
+    return (
+        <div className='mb-10 items-center flex flex-col gap-2'>
+            {/* tracking-wide spacing between words */}
+            <h2 className='font-bold text-4xl tracking-wide'>Search <span className='text-primary'>Doctors</span> </h2>
+            <h2 className='text-gray-500 text-xl'>Search Your Doctor and Book Appointment in One click</h2>
+            <div className="flex w-full mt-3 max-w-sm items-center space-x-2">
+                <Input type="text" placeholder="Search" />
+                <Button type="submit">
+                    <Search  className='h-4 w-4 mr-2' />
+                    Search</Button>
+            </div>
+        </div>
+    )
+}
+
+export default CategorySearch
--- app/page.js
+++ app/page.js
@@ -1,11 +1,14 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
-
+import Hero from "./_components/Hero";
+import CategorySearch from "./_components/CategorySearch";
+// 50:02
 export default function Home() {
   return (
     <div>
-      hello
-      <Button>Btn</Button>
+     <Hero/>
+
+     <CategorySearch/>
     </div>
   );
 }
